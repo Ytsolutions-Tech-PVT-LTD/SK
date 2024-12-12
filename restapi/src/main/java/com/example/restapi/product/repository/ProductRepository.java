52fa99aad246c932135cdcd18d33eb5738f023e6
SK-YTS <yts.java5.level1@gmail.com>
2024-12-12 16:52:25 +0530

    11

--- restapi/src/main/java/com/example/restapi/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restapi.product.repository;
-
-import com.example.restapi.common.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
