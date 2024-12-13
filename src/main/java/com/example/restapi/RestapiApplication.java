200c62e1d74f6289fcfc23aa4f03260d9b85e465
SK-YTS <yts.java5.level1@gmail.com>
2024-12-13 15:50:22 +0530

    g

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -10,7 +10,7 @@ public class RestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
-		System.out.println("abcdef");
+		System.out.println("abcdefg");
 	}
 
 }
